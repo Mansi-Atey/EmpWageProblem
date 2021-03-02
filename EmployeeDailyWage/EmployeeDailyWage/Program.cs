75a47efd5a8d642fe62ab7c3f3e6a4158edfe9a6
Mansi-Atey <mansiatey31@gmail.com>
2021-03-02 17:02:47 +0530

    [mansi]ADD: daily wages of employee

---
+++ EmployeeDailyWage/EmployeeDailyWage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeDailyWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            int dailyHrs;
+            int dailyWage;
+            Random random = new Random();
+            int checkVal = random.Next(0, 2);
+            if (checkVal == isFullTime)
+            {
+                dailyHrs = 8;
+            }
+            else
+            {
+                dailyHrs = 0;
+            }
+            dailyWage = dailyHrs * 20;
+            Console.WriteLine("Daily wage of an employee is:" + dailyWage);
+        }
+    }
+}
