cd49de0b80902dce082e56cc6e72b0c61bfdbf9e
Mansi-Atey <mansiatey31@gmail.com>
2021-03-02 17:11:18 +0530

    [mansi]ADD: parttime wages of employee

--- EmpPartTimeWage/EmpPartTimeWage/Program.cs
+++ EmpPartTimeWage/EmpPartTimeWage/Program.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 
-namespace EmployeeDailyWage
+namespace EmpPartTimeWage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int isFullTime = 1;
+            int isFullTime = 0;
+            int isPartTime = 1;
             int dailyHrs;
             int dailyWage;
+        
             Random random = new Random();
             int checkVal = random.Next(0, 2);
-            if (checkVal == isFullTime)
+            if (checkVal == isPartTime)
             {
                 dailyHrs = 8;
             }
             else
             {
-                dailyHrs = 0;
+                dailyHrs = 16;
             }
             dailyWage = dailyHrs * 20;
-            Console.WriteLine("Daily wage of an employee is:" + dailyWage);
+            Console.WriteLine("Daily wage is:" + dailyWage);
         }
     }
 }
